431e97139a95a577f913ff771f7e3a7fbab55b7e
guanshundai <69504746+guanshundai@users.noreply.github.com>
2022-06-17 16:00:47 +0800

    Delete assets directory

--- assets/Option2.87bbea93.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.145dd137.js";const t={};function e(n,o){return" option2 "}var c=r(t,[["render",e]]);export{c as default};
--- assets/option1.5558cab1.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.145dd137.js";const n={};function o(t,e){return" nav2 option1 "}var c=r(n,[["render",o]]);export{c as default};
